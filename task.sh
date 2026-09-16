4c8cec02269f16ad229c6f428f0fc0294bc37e46
Taras <eostasarat@gmail.com>
2026-09-16 21:54:07 +0300

    done

--- task.sh
+++ task.sh
@@ -1,1 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+
+set -euo pipefail
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db ShopDB > full-backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < full-backup.sql
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db --no-create-info ShopDB > data-backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data-backup.sql
